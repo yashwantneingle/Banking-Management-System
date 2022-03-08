414e7fa5e7f684ee228a9fbaf9ef39254e927bf6
Yashwant Ekanath Ingle <69344258+yashwantneingle@users.noreply.github.com>
2022-03-08 17:47:51 +0530

    Updated Ministatement,transaction and employee.
    Added few instances

--- Banking_database.sql
+++ Banking_database.sql
@@ -14,8 +14,8 @@ CREATE TABLE Admin (
 
 
 INSERT INTO Admin (id, branch_id, name, email_id, password,message) VALUES
-(111, 1 ,'maha_admin', 'admin1@gmail.com', 'admin',""),
-(224, 2 ,'Reliance_admin2', 'admin2@gmail.com', 'admin',""),
+(111, 1 ,'Nagpur_admin', 'admin1@gmail.com', 'admin',""),
+(224, 2 ,'Mumbai_admin2', 'admin2@gmail.com', 'admin',""),
 (312, 3 ,'admin3', 'admin3@gmail.com', 'admin',""),
 (243, 4 ,'admin56', 'admin56@gmail.com', 'admin',""),
 (242, 5 ,'admin4', 'admin4@gmail.com', 'admin',"");
@@ -31,22 +31,23 @@ CREATE TABLE Customer (
   phone int(14) NOT NULL,
   password varchar(20) NOT NULL,
   DOB date NOT NULL,
+  Balance decimal(10,2) NOT NULL,
   address varchar(100) NOT NULL,
   PRIMARY KEY (id , Board_id)
 );
 
 
 INSERT INTO Customer (id, account_no,branch_id, name, email_id, phone, password, DOB, address) VALUES
-(1,'351313513513', 1, 'Avinash', 'avinash@gmail.com', 2147483647, 'pass1234', '1995-12-15', 'Gandhi nagar'),
-(2,'351313513513', 1 ,'Yadnyadeep','yadnya@gmail.com', 999999999, 'pass1234', '1998-12-15', 'Saraswati colony'),
-(3,'351313513513', 2 ,'Vrushabh', 'vrushabhl@gmail.com', 2147483647, 'pass1234', '1995-12-15',  'Main road'),
-(4, '351313513513',2 , 'Vivek', 'vivek@gmail.com', 2147483647, 'pass1234', '1992-12-10', 'Main raod'),
-(5, '351313513513',3 , 'Vinay', 'abc@gmail.com', 2147483647, 'pass1234', '1989-12-08', 'Maharashtra'),
-(6,'351313513513', 3 , 'xyz', 'xyz@gmail.com', 2147483647, 'pass1234', '1989-02-15', 'Maharashtra'),
-(7, '351313513513',4 ,'akash', 'akash@gmail.com', 123456789, 'pass1234', '1989-02-15', 'Maharshtra'),
-(757, '351313513513',4 ,'Rahul', 'rahul@gmail.com', 123456789, 'pass1234', '1989-12-15', 'India'),
-(7,'351313513513', 5 ,'Ram', 'ram@gmail.com', 123456789, 'pass1234', '1996-01-15',  'Maharastra'),
-(75,'351313513513', 5 ,'Yashwant', 'yashwant@gmail.com', 123456789, 'pass1234', '1989-01-15',  'Maharashtra');
+(1,'351313513513', 1, 'Avinash', 'avinash@gmail.com', 2147483647, 'pass1234', '1995-12-15', '1200.00','Gandhi nagar'),
+(2,'351313513513', 1 ,'Yadnyadeep','yadnya@gmail.com', 999999999, 'pass1234', '1998-12-15', '1200.00','Saraswati colony'),
+(3,'351313513513', 2 ,'Vrushabh', 'vrushabhl@gmail.com', 2147483647, 'pass1234', '1995-12-15','1200.00','Main road'),
+(4, '351313513513',2 , 'Vivek', 'vivek@gmail.com', 2147483647, 'pass1234', '1992-12-10', '1200.00','Main raod'),
+(5, '351313513513',3 , 'Vinay', 'abc@gmail.com', 2147483647, 'pass1234', '1989-12-08', '1200.00','Maharashtra'),
+(6,'351313513513', 3 , 'xyz', 'xyz@gmail.com', 2147483647, 'pass1234', '1989-02-15', '1200.00','Maharashtra'),
+(7, '351313513513',4 ,'akash', 'akash@gmail.com', 123456789, 'pass1234', '1989-02-15','1200.00', 'Maharshtra'),
+(757, '351313513513',4 ,'Rahul', 'rahul@gmail.com', 123456789, 'pass1234', '1989-12-15', '1200.00','India'),
+(7,'351313513513', 5 ,'Ram', 'ram@gmail.com', 123456789, 'pass1234', '1996-01-15', '1200.00', 'Maharastra'),
+(75,'351313513513', 5 ,'Yashwant', 'yashwant@gmail.com', 123456789, 'pass1234', '1989-01-15', '120000.00', 'Maharashtra');
 
 
 
@@ -54,7 +55,7 @@ INSERT INTO Customer (id, account_no,branch_id, name, email_id, phone, password,
 CREATE TABLE Ministatement (
   Account_no int(14) NOT NULL,
   branch_id int(14) NOT NULL,
-  paymnent_date date NOT NULL,
+  payment_date date NOT NULL,
   transaction_id int(14) NOT NULL,
   Amount decimal(10,2) NOT NULL,
   transaction_type varchar(10) NOT NULL,
@@ -63,17 +64,10 @@ CREATE TABLE Ministatement (
 
 
 
-INSERT INTO Ministatement (Account_no, branch_id,payment_date, Amount, transaction_type,opening) VALUES
-(1, 1, '2021-12-01', 2545,'credited',3654354,31315),
-(2, 1,  '2021-09-01', 23554,'debited'),
-(3, 2, '2021-10-01', 846,'2021-10-31','debited'),
-(4, 2, '2021-10-01', 65464,'2021-10-31','credited'),
-(5, 3, '2021-10-01', 54534,'2021-10-31','debited'),
-(6, 3, '2021-10-01', 6544,'2021-10-31','debited'),
-(7, 4, '2021-11-01', 354,'2021-12-01','credited'),
-(757, 4,'2021-11-01', 3645,'2021-12-01','debited'),
-(7, 5,  '2021-11-01', 321,'2021-12-01','credited'),
-(75, 5,  '2020-11-01', 354,'2020-12-01','credited');
+INSERT INTO Ministatement (Account_no, branch_id,payment_date, transaction_id, Amount, transaction_type,opening) VALUES
+('351313513513',1,'2021-12-01',3535135153,'1000.00',"credited",'12000.00')
+('351313513513',1,'2021-12-05',3535185153,'1200.00',"credited",'13000.00')
+('351313513513',1,'2022-01-01',3535175153,'1200.00',"credited",'14000.00');
 
 
 
@@ -96,7 +90,7 @@ INSERT INTO query (id, branch_id, complaint, response) VALUES
 
 CREATE TABLE transaction (
   id int(14) NOT NULL,
-  Board_id int(14) NOT NULL,
+  Branch_id int(14) NOT NULL,
   Payable_amount decimal(10,2) NOT NULL,
   Pay_date date NOT NULL,
   status varchar(20) NOT NULL,
@@ -104,7 +98,7 @@ CREATE TABLE transaction (
 
 
 
-INSERT INTO transaction (id, Board_id, Payable_amount, Pay_date, status) VALUES
+INSERT INTO transaction (id, Branch_id, Payable_amount, Pay_date, status) VALUES
 (1, 1, '1024.00', '2020-01-06', 'PROCESSED'),
 (2, 1, '1400.00', '2020-10-10', 'PROCESSED'),
 (4, 2, '1206.00', '2020-10-10', 'PROCESSED'),
@@ -117,7 +111,7 @@ INSERT INTO transaction (id, Board_id, Payable_amount, Pay_date, status) VALUES
 
 CREATE TABLE Employee (
   id int(14) NOT NULL,
-  Board_id int(14) NOT NULL,
+  Branch_id int(14) NOT NULL,
   name varchar(40) NOT NULL,
   email_id varchar(40) NOT NULL,
   phone int(15) NOT NULL,
@@ -126,7 +120,7 @@ CREATE TABLE Employee (
 
 
 
-INSERT INTO Employee (id, Board_id, name, email_id, phone, password) VALUES
+INSERT INTO Employee (id, Branch_id, name, email_id, phone, password) VALUES
 (3551, 1, 'Aman', 'aman@gmail.com', 2147483647, 'pass'),
 (3852, 1 ,'Abhishek', 'abhishek@gmail.com', 999999999, 'pass'),
 (68683, 2 ,'Ana', 'ana@gmail.com', 2147483123, 'pass'),
